1253fdd0359f19462e4e1426480c1bad94e53440
Jon Jensen <github@id.xjensen.com>
2023-11-07 16:14:47 -0800

    Change deploy ref

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-# CODEBUILD_WEBHOOK_BASE_REF="refs/heads/the-branch"
-# CODEBUILD_WEBHOOK_TRIGGER="pr/5"
+CODEBUILD_WEBHOOK_HEAD_REF="refs/heads/the-branch"
+CODEBUILD_WEBHOOK_TRIGGER="pr/5"
+BENEFITS_RECS_DEPLOY_OUTPUT="✓ Success! Deployed app in 55.815 seconds\n    https://7ksmy2xna5.execute-api.us-west-1.amazonaws.com\n"
 
 # Get the name for our arc.codes instance.
-if [ -n $CODEBUILD_WEBHOOK_BASE_REF ]
+if [ -n $CODEBUILD_WEBHOOK_HEAD_REF ]
 then
   # Strip "refs/heads/" from the GitHub ref.
-  BENEFITS_RECS_INSTANCE_NAME=$(echo ${CODEBUILD_WEBHOOK_BASE_REF#refs\/heads\/} | sed 's|[^A-Za-z0-9-]|-|g' | sed -E 's|-*([A-Za-z0-9]*.*[A-Za-z0-9]+)-*|\1|')
+  BENEFITS_RECS_INSTANCE_NAME=$(echo ${CODEBUILD_WEBHOOK_HEAD_REF#refs\/heads\/} | sed 's|[^A-Za-z0-9-]|-|g' | sed -E 's|-*([A-Za-z0-9]*.*[A-Za-z0-9]+)-*|\1|')
 else 
   # Provide this default if we're testing and the PR info isn't available.
   BENEFITS_RECS_INSTANCE_NAME="codebuild-test"
@@ -33,10 +34,5 @@ echo "Git branch: $BENEFITS_RECS_INSTANCE_NAME"
 #  exit $BENEFITS_RECS_DEPLOY_STATUS
 #fi
 
-BENEFITS_RECS_DEPLOY_OUTPUT="✓ Success! Deployed app in 55.815 seconds
-
-    https://7ksmy2xna5.execute-api.us-west-1.amazonaws.com
-"
-
 BENEFITS_RECS_ENDPOINT_URL=$(echo "\001$BENEFITS_RECS_DEPLOY_OUTPUT\002" | tail -n 2 | xargs)
-echo "\001Endpoint: $BENEFITS_RECS_ENDPOINT_URL\002"
+echo "Endpoint: $BENEFITS_RECS_ENDPOINT_URL"
