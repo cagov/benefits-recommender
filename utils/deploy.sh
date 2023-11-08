800002f053b08a14b727b958a65ae6c7c2243066
Jon Jensen <github@id.xjensen.com>
2023-11-07 18:06:08 -0800

    Turn on deployment from script

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
+# Useful variables for development...
 # CODEBUILD_WEBHOOK_HEAD_REF="refs/heads/the-branch"
 # CODEBUILD_WEBHOOK_TRIGGER="pr/5"
-BENEFITS_RECS_DEPLOY_OUTPUT="✓ Success! Deployed app in 55.815 seconds\n    https://7ksmy2xna5.execute-api.us-west-1.amazonaws.com\n"
+# BENEFITS_RECS_DEPLOY_OUTPUT="✓ Success! Deployed app in 55.815 seconds\n    https://7ksmy2xna5.execute-api.us-west-1.amazonaws.com\n"
 
 # Get the name for our arc.codes instance.
-if [ -n $CODEBUILD_WEBHOOK_HEAD_REF ]
+if [ -z $CODEBUILD_WEBHOOK_HEAD_REF ]
 then
-  # Strip "refs/heads/" from the GitHub ref.
-  BENEFITS_RECS_INSTANCE_NAME=$(echo ${CODEBUILD_WEBHOOK_HEAD_REF#refs\/heads\/} | sed 's|[^A-Za-z0-9-]|-|g' | sed -E 's|-*([A-Za-z0-9]*.*[A-Za-z0-9]+)-*|\1|')
-else 
   # Provide this default if we're testing and the PR info isn't available.
   BENEFITS_RECS_INSTANCE_NAME="codebuild-test"
+else
+  # Strip "refs/heads/" from the GitHub ref.
+  BENEFITS_RECS_INSTANCE_NAME=$(echo ${CODEBUILD_WEBHOOK_HEAD_REF#refs\/heads\/} | sed 's|[^A-Za-z0-9-]|-|g' | sed -E 's|-*([A-Za-z0-9]*.*[A-Za-z0-9]+)-*|\1|') 
 fi
 
 if [ -n $CODEBUILD_WEBHOOK_TRIGGER ]
 then
+  BENEFITS_RECS_PR_NUMBER="9999"
+else
   # strip "pr/" from webhook trigger
   BENEFITS_RECS_PR_NUMBER=${CODEBUILD_WEBHOOK_TRIGGER#pr\/}
 fi
@@ -24,15 +27,25 @@ echo "PR number: $BENEFITS_RECS_PR_NUMBER"
 echo "Git branch: $BENEFITS_RECS_INSTANCE_NAME"
 
 # Attempt to deploy. tee streams the output.
-#BENEFITS_RECS_DEPLOY_OUTPUT=$(npx arc deploy | tee /dev/stderr)
-#BENEFITS_RECS_DEPLOY_STATUS=$?
+BENEFITS_RECS_DEPLOY_OUTPUT=$(npx arc deploy --name $BENEFITS_RECS_INSTANCE_NAME | tee /dev/stderr)
+BENEFITS_RECS_DEPLOY_STATUS=$?
 
 # If deployment failed, let's bail out here.
-#if [ $BENEFITS_RECS_DEPLOY_STATUS -gt 0 ]
-#then
-#  echo "\001$BENEFITS_RECS_DEPLOY_OUTPUT\002"
-#  exit $BENEFITS_RECS_DEPLOY_STATUS
-#fi
+if [ $BENEFITS_RECS_DEPLOY_STATUS -gt 0 ]
+then
+  echo "\001$BENEFITS_RECS_DEPLOY_OUTPUT\002"
+  exit $BENEFITS_RECS_DEPLOY_STATUS
+fi
 
 BENEFITS_RECS_ENDPOINT_URL=$(echo "\001$BENEFITS_RECS_DEPLOY_OUTPUT\002" | tail -n 2 | xargs)
 echo "Endpoint: $BENEFITS_RECS_ENDPOINT_URL"
+
+npm run widget:build:preview -- $BENEFITS_RECS_INSTANCE_NAME $BENEFITS_RECS_PR_NUMBER $BENEFITS_RECS_ENDPOINT_URL
+
+echo "\n> Uploading to S3."
+aws s3 sync ./widget/dist s3://staging.cdn.innovation.ca.gov/br/pr/$BENEFITS_RECS_INSTANCE_NAME --follow-symlinks
+
+echo "\n> Sending invalidation to Cloudfront cache."
+AWS_PAGER="" aws cloudfront create-invalidation --distribution-id EPGKQG9OR4C9S --paths "/br/*"
+
+echo "\nPreview: https://staging.cdn.innovation.ca.gov/br/pr/$BENEFITS_RECS_INSTANCE_NAME/preview/index.html"
