f2d68dac5bbbcb77548053f6520e8e1401bcf5b1
Jon Jensen <github@id.xjensen.com>
2023-11-08 18:53:32 -0800

    Remove ANSI control chars

--- utils/deploy-pr.sh
+++ utils/deploy-pr.sh
@@ -38,7 +38,7 @@ then
   exit $BENEFITS_RECS_DEPLOY_STATUS
 fi
 
-BENEFITS_RECS_ENDPOINT_URL=$(echo "\001$BENEFITS_RECS_DEPLOY_OUTPUT\002" | tail -n 2 | xargs)
+BENEFITS_RECS_ENDPOINT_URL=$(echo "\001$BENEFITS_RECS_DEPLOY_OUTPUT\002" | tail -n 2 | xargs | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")
 echo "Endpoint: $BENEFITS_RECS_ENDPOINT_URL"
 
 echo "\n> Generating front-end preview assets."
